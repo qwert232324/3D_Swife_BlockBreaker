8807ce22272874b19d527e90aca3c7fc03d71816
qwert232323 <qwert232324@naver.com>
2022-07-12 19:00:05 +0900

    v_0.1
    기본 틀 제작
    기본 GameObject 구현
    공 닿으면 색 변환 -> 파괴 블록 구현

---
+++ 3D_Swife_BlockBreaker/Assets/Scripts/Block.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    int hitCount= 9;
+    MeshRenderer mesh;
+    public Material[] colors = new Material[9];
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+    private void OnEnable()
+    {
+        hitCount = GameManager.instance.stage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "BALL")
+        {
+            hitCount--;            
+            if (hitCount <= 0) Break();
+            mesh.material = colors[hitCount - 1];
+        }
+    }
+    private void Break()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+}
---
+++ 3D_Swife_BlockBreaker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int stage;
+
+    void Start()
+    {
+        instance = this;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
